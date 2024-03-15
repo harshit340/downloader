e345b7245e1c781110c92cac1bcc51f7f6bb84b6
harshit <harshitshrivastava340@gmail.com>
2024-03-15 22:27:08 +0530

    its my initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+ import React from 'react';
+import Nav from './component/Nav';
+import Home from './component/Home';
+import Facebook from './component/Facebook';
+import Instagram from './component/Instagram';
+import Youtube from './component/Youtube';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const  App = ()=> {
+
+  
+ /*  const [ category ,setCategory] = React.useState(Home);
+
+  const filterItem = (category) =>{
+  if(category==="instagram"){
+    setCategory(Instagram);
+    return ;
+  }
+  else if(category==="facebook"){
+   setCategory(Facebook);
+   return ;
+  }
+  else if(category==="youtube"){
+    setCategory(Youtube);
+    return ;
+  } */
+
+  let component 
+  
+  switch(window.location.pathname){
+    case "/":
+      component = <Home/>
+      break;
+    case "/Home":
+      component = <Home/>
+      break;
+    case "/Facebook":
+      component =<Facebook/>
+     break;
+    case "/Instagram":
+      component = <Instagram/>
+      break;
+    case "/Youtube":
+      component = <Youtube/>
+      break;
+  }
+
+
+
+  return (
+   <>
+
+   <Nav/>
+   {component}
+
+   </>
+  );
+
+}
 export default App;
+ 
+
